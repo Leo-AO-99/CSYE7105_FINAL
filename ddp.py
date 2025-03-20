ad6a7f268e02fe332dde9501d9e3bf1cd605d745
LiAo <1835702163@qq.com>
2025-03-19 20:02:28 -0400

    ddp update

--- ddp.py
+++ ddp.py
@@ -16,25 +16,12 @@ from ddpm.config import LayerConfig, cifar10_config
 from ddpm.data import get_cifar10_datasets
 from ddpm.diffusion_model import DiffusionModel
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Train DDPM with DDP')
-    parser.add_argument('--max_epochs', type=int, default=None, help='Maximum number of epochs')
-    parser.add_argument('--batch_size', type=int, default=None, help='Batch size per GPU')
-    parser.add_argument('--load_cpt', type=str, default=None, 
-                        help='load checkpoint path')
-    return parser.parse_args()
+
 
 _config.DEBUG = False
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device_count = torch.cuda.device_count()
 
-# max_epochs = 500
-max_epochs = 1000
-batch_size = cifar10_config.batch_size
-
-
-each_epochs = max_epochs // torch.cuda.device_count()
-
-learning_rate = 1e-4
 
 # train_loader, test_loader = get_cifar10_dataloaders(batch_size=batch_size)
 
@@ -63,18 +50,20 @@ def train(rank, world_size, args):
     train_dataset, test_dataset = get_cifar10_datasets()
     
     if args.load_cpt is not None:
-        checkpoint = torch.load(args.load_cpt, map_location=f'cuda:{rank}')
+        checkpoint = torch.load(args.load_cpt, map_location=f'cuda:{rank}', weights_only=False)
         model = DiffusionModel(checkpoint['config']).to(rank)
         model.load_state_dict(checkpoint['model'])
         
         model_ema = DiffusionModel(checkpoint['config']).to(rank)
         model_ema.load_state_dict(checkpoint['model'])  # 用相同的权重初始化EMA模型
         model_ema.eval()
+        cur_config = checkpoint['config']
     else:
         model = DiffusionModel(cifar10_config).to(rank)
         model_ema = DiffusionModel(cifar10_config).to(rank)
         model_ema.load_state_dict(model.state_dict())
         model_ema.eval()
+        cur_config = cifar10_config
 
     
 
@@ -83,14 +72,17 @@ def train(rank, world_size, args):
 
 
     sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank)
-    dataloader = DataLoader(train_dataset, batch_size=batch_size, sampler=sampler, num_workers=4, pin_memory=True)
+    dataloader = DataLoader(train_dataset, batch_size=args.batch_size, sampler=sampler, num_workers=4, pin_memory=True)
+    if rank == 0:
+        print(f"dataset length: {len(train_dataset)}")
+    print(f"Rank {rank} dataloader length: {len(dataloader)}")
     
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
     # CosineAnnealingLR will decay the LR smoothly over max_epochs
-    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=each_epochs)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.max_epochs)
 
-    for epoch in range(each_epochs):
+    for epoch in range(args.max_epochs):
         start_time = time.time()
         sampler.set_epoch(epoch)
         for images, labels in dataloader:
@@ -104,12 +96,12 @@ def train(rank, world_size, args):
             optimizer.step()
             update_ema(model, model_ema)
         scheduler.step()
-        
+
         model_ema.eval()
         end_time = time.time()
         print(f"Rank {rank} Epoch {epoch} took {end_time - start_time:.3f} seconds, ls: {loss.item()}")
-        
-        if rank == 0 and (epoch + 1) % 25 == 0:
+
+        if rank == 0 and (epoch + 1) % args.interval == 0:
             save_dict = {
                 "model": model.module.state_dict(),
                 "config": cifar10_config,
@@ -117,15 +109,19 @@ def train(rank, world_size, args):
             torch.save(save_dict, f"{cpt_prefix}_cpt/epoch_{epoch}.pth")
 
     dist.destroy_process_group()
+    
+    
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train DDPM with DDP')
+    parser.add_argument('--max_epochs', type=int, default=1000, help='Maximum number of epochs')
+    parser.add_argument('--batch_size', type=int, default=None, help='Batch size per GPU')
+    parser.add_argument('--load_cpt', type=str, default=None, help='load checkpoint path')
+    parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate')
+    parser.add_argument('--interval', type=int, default=25, help='Interval of saving checkpoint')
+    return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_args()
-    
-    if args.max_epochs is not None:
-        max_epochs = args.max_epochs
-        each_epochs = max_epochs // torch.cuda.device_count()
-    if args.batch_size is not None:
-        batch_size = args.batch_size
     
     ss_time = time.time()
     
